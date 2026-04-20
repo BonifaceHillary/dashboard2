f45da5d75980c7755022fe5bee70d8344e1f99ba
Boniface Hillary <nkasiboniface@gail.com>
2026-04-20 14:06:36 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { createBrowserRouter, RouterProvider, Router  } from "react-router-dom";
+import Home from "./pages/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  }
+])
+
 function App() {
-  return <div className='app'>HillsTech</div>;
+  return <div className='app'>
+    <RouterProvider router={router}/>
+  </div>
 
    
 }
