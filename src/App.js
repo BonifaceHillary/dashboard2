0031c95de077e183f2bace0f56952f38a1416a42
Boniface Hillary <nkasiboniface@gail.com>
2026-04-20 15:27:23 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import { createBrowserRouter, RouterProvider, Router  } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import Home from "./pages/home/Home";
-
+import Customers from "./pages/customers/Customers"
+import Product from "./pages/product/Product"
+import New from "./pages/new/New"
+import Single from "./pages/single/Single"
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>
+  },
+
+   {
+    path: "/customers/",
+    element: <Customers/>
+  },
+
+   {
+    path: "/products",
+    element: <Product/>
+  },
+
+   {
+    path: "/product/:Product/new",
+    element: <New/>
+  },
+
+  {
+    path: "/customers/:customerId/new",
+    element: <New/>
+  },
+
+   {
+    path: "/customers/:CustomersId",
+    element: <Single/>
+  },
+
+   {
+    path: "/product/:ProductId",
+    element: <Single/>
   }
 ])
 
