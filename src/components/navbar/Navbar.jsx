3c8cb439868a8df18a447341afe0624e4dd4c137
Boniface Hillary <nkasiboniface@gail.com>
2026-06-13 21:31:34 +0100

    initial commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+
 import "./navbar.scss"
 
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+
+import Switch from '@mui/material/Switch';
+
+import FullscreenOutlinedIcon from '@mui/icons-material/FullscreenOutlined';
+
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+
 
 const Navbar = () => {
   return (
@@ -12,10 +26,34 @@ const Navbar = () => {
           <SearchOutlinedIcon />
 
         </div>
-        <div className="items">items</div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className='icon' />
+            <span>English</span>
+          </div>
+          <div className="item">
+            <Switch style={{color: "#210876"}} className='icon' />
+          </div>
+          <div className="item">
+            <FullscreenOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <NotificationsActiveOutlinedIcon className='icon'/>
+            <div className="counter">3</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon'/>
+            <div className="counter">3</div>
+
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className='icon'/>
+
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar 
