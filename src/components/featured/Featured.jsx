5212e8f734ff073cb3609c2c57f9d9b307d993fe
Boniface Hillary <nkasiboniface@gail.com>
2026-06-16 20:58:58 +0100

    initial commit

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,23 +2,99 @@ import React from 'react'
 import "./featured.scss"
 
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+
 // import { CircularProgress } from '@mui/material';
+
+// import { Box, Typography, LinearProgress } from '@mui/material';
+
+
+import CircularProgress from '@mui/material/CircularProgress';
+
+import Typography from '@mui/material/Typography';
 
 const Featured = () => {
   return (
+
+
+
+
     <div className='featured'>
       <div className="top">
         <h1 className="title">total revenue</h1>
         <MoreVertIcon fontSize='small' />
       </div>
       <div className="bottom">
-        <div className="featuredchart">
+         <div className="featuredchart"> 
+
+
+
+
+
+          <div style={{ position: "relative", display: "inline-flex" }}>
+  <CircularProgress
+    variant="determinate"
+    value={70}
+    size={120}
+  />
+  <Typography
+    sx={{
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+    }}
+  >
+    70%
+  </Typography>
+</div>
+
+
+
 
         
           
         </div> 
       </div>
-    </div>
+   </div>
+
+
+
+
+
+
+
+// <div className='featured'>
+//   <div className="top">
+//     <h1 className="title">Total Revenue</h1>
+//     <MoreVertIcon fontSize="small" />
+//   </div>
+
+//   <div className="bottom">
+//     <div className="featuredChart">
+//       <div style={{ position: "relative", display: "inline-flex" }}>
+//         <CircularProgress
+//           variant="determinate"
+//           value={70}
+//           size={120}
+//         />
+//         <Typography
+//           sx={{
+//             position: "absolute",
+//             top: "50%",
+//             left: "50%",
+//             transform: "translate(-50%, -50%)",
+//           }}
+//         >
+//           70%
+//         </Typography>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+
+
+
   )
 }
 
